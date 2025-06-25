244dc5dfbcd91eb7731e036f4728c0772f60a97b
JeongUpGi <dkdfl1201@gmail.com>
2025-06-25 18:54:43 +0900

    변수명 변경 (camelCase -> snake_case)

--- models/playLogModel.js
+++ models/playLogModel.js
@@ -11,10 +11,10 @@ exports.insertPlayLog = (
 ) => {
   console.log("playedAt");
   const sql = `
-      INSERT INTO play_logs (video_id, title, artist, thumbnail_url, last_played_date, playCount)
+      INSERT INTO play_logs (video_id, title, artist, thumbnail_url, last_played_date, play_count)
       VALUES (?, ?, ?, ?, ?, 1)
       ON DUPLICATE KEY UPDATE
-        playCount = playCount + 1,
+        play_count = play_count + 1,
         last_played_date = VALUES(last_played_date),
         title = VALUES(title),
         artist = VALUES(artist),
@@ -26,9 +26,9 @@ exports.insertPlayLog = (
 // 로그 조회 (SELECT)
 exports.selectPlayLogs = (callback) => {
   const sql = `
-    SELECT id, video_id, title, artist, playCount, last_played_date, thumbnail_url
+    SELECT id, video_id, title, artist, play_count, last_played_date, thumbnail_url
     FROM play_logs
-    ORDER BY playCount DESC, last_played_date DESC
+    ORDER BY play_count DESC, last_played_date DESC
     LIMIT 100
   `;
   db.query(sql, callback);
