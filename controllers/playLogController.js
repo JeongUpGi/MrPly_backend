0ec593a53e9a1ec520639a652542687c078a5255
JeongUpGi <dkdfl1201@gmail.com>
2025-06-25 18:50:08 +0900

    동영상 썸네일 이미지 또한 req, resdata에 추가

--- controllers/playLogController.js
+++ controllers/playLogController.js
@@ -3,22 +3,29 @@ const { insertPlayLog, selectPlayLogs } = require("../models/playLogModel");
 // 로그 저장
 exports.savePlayLog = (req, res) => {
   console.log("req ===> ", req.body);
-  const { videoId, title, artist, lastPlayedDate } = req.body;
-  if (!videoId || !title || !artist) {
+  const { videoId, title, artist, thumbnailUrl, playedAt } = req.body;
+  if (!videoId || !title || !artist || !thumbnailUrl) {
     return res
       .status(400)
-      .json({ error: "videoId, title, artist는 필수입니다." });
+      .json({ error: "videoId, title, artist, thumbnailUrl는 필수입니다." });
   }
-  // playedAt이 없으면 현재 시간 사용
-  const playedAtValue = lastPlayedDate || new Date();
 
-  insertPlayLog(videoId, title, artist, playedAtValue, (err, result) => {
-    if (err) {
-      console.error("DB insert error:", err);
-      return res.status(500).json({ error: "DB 저장 실패" });
+  const playedAtValue = new Date().toISOString().slice(0, 19).replace("T", " ");
+
+  insertPlayLog(
+    videoId,
+    title,
+    artist,
+    thumbnailUrl,
+    playedAtValue,
+    (err, result) => {
+      if (err) {
+        console.error("DB insert error:", err);
+        return res.status(500).json({ error: "DB 저장 실패" });
+      }
+      res.status(201).json({ message: "로그 저장 성공", id: result.insertId });
     }
-    res.status(201).json({ message: "로그 저장 성공", id: result.insertId });
-  });
+  );
 };
 
 // 로그 조회
--- models/playLogModel.js
+++ models/playLogModel.js
@@ -1,23 +1,32 @@
 const db = require("./db");
 
 // 로그 저장 (INSERT)
-exports.insertPlayLog = (videoId, title, artist, playedAt, callback) => {
+exports.insertPlayLog = (
+  videoId,
+  title,
+  artist,
+  thumbnailUrl,
+  playedAt,
+  callback
+) => {
+  console.log("playedAt");
   const sql = `
-    INSERT INTO play_logs (video_id, title, artist, playCount, last_played_date)
-    VALUES (?, ?, ?, 1, ?)
-    ON DUPLICATE KEY UPDATE
-      playCount = playCount + 1,
-      last_played_date = VALUES(last_played_date),
-      title = VALUES(title),
-      artist = VALUES(artist)
-  `;
-  db.query(sql, [videoId, title, artist, playedAt], callback);
+      INSERT INTO play_logs (video_id, title, artist, thumbnail_url, last_played_date, playCount)
+      VALUES (?, ?, ?, ?, ?, 1)
+      ON DUPLICATE KEY UPDATE
+        playCount = playCount + 1,
+        last_played_date = VALUES(last_played_date),
+        title = VALUES(title),
+        artist = VALUES(artist),
+        thumbnail_url = VALUES(thumbnail_url)
+    `;
+  db.query(sql, [videoId, title, artist, thumbnailUrl, playedAt], callback);
 };
 
 // 로그 조회 (SELECT)
 exports.selectPlayLogs = (callback) => {
   const sql = `
-    SELECT id, video_id, title, artist, playCount, last_played_date
+    SELECT id, video_id, title, artist, playCount, last_played_date, thumbnail_url
     FROM play_logs
     ORDER BY playCount DESC, last_played_date DESC
     LIMIT 100
